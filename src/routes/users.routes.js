dc27ab458ec99bf00f9b3202096aaaf65b201354
Bruno <brunoalmeida0805@gmail.com>
2023-05-24 23:49:29 -0300

    atualização do server

---
+++ src/routes/users.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+function myMiddleware(request, response, next) {
+  next();
+}
+
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
+
+module.exports = usersRoutes;
