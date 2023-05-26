0a23d0f2b84e00bc33d62a4d8ddebaa4a5b31af9
Bruno <brunoalmeida0805@gmail.com>
2023-05-25 21:08:14 -0300

    cadastramento de usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,10 +1,24 @@
 const AppError = require ("../utils/AppError");
 
+const sqliteConnection = require ("../database/sqlite");
+
 class UsersController {
-  create (request, response){
+  async create (request, response){
     const { name, email, password } = request.body;
 
-    response.status(201).json({ name, email, password });
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExists){
+      throw new AppError("este email já está em uso.");
+    }
+    await database.run (
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+      [name, email, password]
+    );
+
+    return response.status(201).json();
+
   }
 }
 
