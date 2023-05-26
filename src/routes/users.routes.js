d91c115112f9d1fb7197bf7770212ae75a398d45
Bruno <brunoalmeida0805@gmail.com>
2023-05-25 22:08:17 -0300

    atualização do usuário

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -11,5 +11,6 @@ function myMiddleware(request, response, next) {
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
